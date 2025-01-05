aab10715ec56838d774532d1625c68cd7ad1a792
shashank chauhan <shashankchauhan8577@gmail.com>
2025-01-05 05:31:18 +0530

    requirements added

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # object-detection-project
+
+# how to run:
+
+```bash
+conda create -n objectdetect python=3.7 -y
+```
+
+```bash
+conda activate objectdetect
+```
+
+```bash
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='objectdetection',
+    version='0.0.0',
+    author='shashank chauhan',
+    author_email='shashankchauhan8577@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
